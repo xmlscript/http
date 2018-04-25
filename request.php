c9d5575ecc37de9d2e4f4d9e034337286bf51554
xmlscript <xmlscript@gmail.com>
2018-04-25 17:20:42 +0800

    dev: test

--- request.php
+++ request.php
@@ -84,7 +84,9 @@ class request{
 
 
   final function query(array $q):self{
-    return $this->setopt(CURLOPT_URL, strstr(curl_getinfo($this->handle,CURLINFO_EFFECTIVE_URL).'?','?',true).'?'.http_build_query($q,'','&',PHP_QUERY_RFC3986));
+    $this->setopt(CURLOPT_URL, strstr(curl_getinfo($this->handle,CURLINFO_EFFECTIVE_URL).'?','?',true).'?'.http_build_query($q,'','&',PHP_QUERY_RFC3986));
+    throw new \Error(curl_getinfo($this->handle, CURLINFO_EFFECTIVE_URL));
+    return $this;
   }
 
 
