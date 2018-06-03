cd65ea93c64fba0d2a35a0ce92bc5515f0606d48
xmlscript <xmlscript@gmail.com>
2018-06-03 17:13:41 +0800

    m

--- ip.php
+++ ip.php
@@ -9,9 +9,6 @@ class ip{
   }
 
 
-  /**
-   * 生成一个代理信任链，第一条绝对可信
-   */
   function __debugInfo():array{
     return [-1=>$_SERVER['REMOTE_ADDR']??'']+array_reverse(explode(', ', $_SERVER['HTTP_X_FORWARDED_FOR']??''));
   }
@@ -19,11 +16,20 @@ class ip{
 
   function __toString():string{
     foreach($this->__debugInfo() as $ip)
-      if(filter_var($ip, FILTER_VALIDATE_IP,FILTER_FLAG_NO_RES_RANGE)&&array_search($ip,$this->trust)!==false)
+      if($this->valid($ip)&&array_search($ip,$this->trust)!==false)
         continue;
       else
         return $ip;
     return $ip;
+  }
+
+  private function valid(string $ip):bool{
+    return filter_var($ip, FILTER_VALIDATE_IP,FILTER_FLAG_NO_RES_RANGE);
+  }
+
+
+  static function ipecho():?string{
+    return $this->valid($ip=request::url('http://ipecho.net/plain')->fetch()->body())?$ip:null;
   }
 
 }
